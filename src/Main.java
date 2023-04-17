e86f4ee26a57e552d2387c8e68171d0c21a6f655
Pavel Zimogliad <3abodskoy123@gmail.com>
2023-04-18 01:14:24 +0300

    Исправление ошибок дома 17.04

--- src/Main.java
+++ src/Main.java
@@ -11,23 +11,20 @@ public class Main {
         String num_1 = s[0];
         String num_2 = s[2];
         String znak = s[1];
-
-
-        if ((s.length == 3) & (1 <= Integer.parseInt(num_1)) & (10 >= Integer.parseInt(num_1)) & (1 <= Integer.parseInt(num_2)) & (10 >= Integer.parseInt(num_2))) {
+        if ((s.length == 3) & (znak.matches("^[+,-,*,/]+$"))) {
             if (num_1.matches("^[I,II,III,IV,V,VI,VII,VIII,IX,X]+$") & num_2.matches("^[I,II,III,IV,V,VI,VII,VIII,IX,X]+$")) {
                 System.out.println(getRomeRusult(num_1, num_2, znak));
             } else {
                 System.out.println(getResult(num_1, num_2, znak));
             }
-            return  ;
         }
+        throw new Exception();
     }
 
-
-    static int getResult(String num_1, String num_2, String znak) {
+    static int getResult(String num_1, String num_2, String znak) throws Exception {
         int a = Integer.parseInt(num_1);
         int b = Integer.parseInt(num_2);
-        if ((a >= 0 & a <= 10) & (b >= 0 & b <= 10)) {
+        if ((a >= 1 & a <= 10) & (b >= 1 & b <= 10)) {
             int res = 0;
             switch (znak) {
                 case "-":
@@ -45,11 +42,10 @@ public class Main {
             }
             return res;
         }
-        return -1;
+        throw new Exception();
     }
-
     static String getRomeRusult(String num_1, String num_2, String znak) {
-        String[] roman = {"В римской системе нет нуля или отрицательных значений", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+        String[] roman = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
                 "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
                 "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
                 "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
@@ -57,45 +53,25 @@ public class Main {
                 "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
                 "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
         };
+        String[] numRoman = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        int a = Integer.parseInt(valueOf(Arrays.asList(numRoman).indexOf(num_1)));
+        int b = Integer.parseInt(valueOf(Arrays.asList(numRoman).indexOf(num_2)));
         int total = 1;
         switch (znak) {
             case "-":
-                total = Integer.parseInt(valueOf(Arrays.asList(roman).indexOf(num_1))) - Integer.parseInt(valueOf(Arrays.asList(roman).indexOf(num_2)));
+                total = a - b;
                 break;
             case "+":
-                total = Integer.parseInt(valueOf(Arrays.asList(roman).indexOf(num_1))) + Integer.parseInt(valueOf(Arrays.asList(roman).indexOf(num_2)));
-
+                total = a + b;
                 break;
             case "*":
-                total = Integer.parseInt(valueOf(Arrays.asList(roman).indexOf(num_1))) * Integer.parseInt(valueOf(Arrays.asList(roman).indexOf(num_2)));
+                total = a * b;
                 break;
             case "/":
-                total = Integer.parseInt(valueOf(Arrays.asList(roman).indexOf(num_1))) / Integer.parseInt(valueOf(Arrays.asList(roman).indexOf(num_2)));
+                total = a / b;
                 break;
         }
         String totalRoman = copyValueOf(Arrays.asList(roman).get(total).toCharArray());
         return totalRoman;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
