0ed5572838e45b4c2c9db63c9fc856beae0ab863
Pavel Zimogliad <3abodskoy123@gmail.com>
2023-04-19 21:59:27 +0300

    Исправление ошибок дома 19.04 Готово.

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,35 @@ import static java.lang.String.copyValueOf;
 import static java.lang.String.valueOf;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
+        System.out.println("Введите арифметическое выражение (пример: 2 + 2 или II + II)");
         Scanner scanner = new Scanner(System.in);
-        String[] s = scanner.nextLine().split(" ");
+        String input = scanner.nextLine();
+        String calc = calc(input);
+        System.out.println("Ответ: " + calc);
+        scanner.close();
+    }
+
+    public static String calc(String input) throws Exception {
+        String[] s = input.split(" ");
         String num_1 = s[0];
         String num_2 = s[2];
         String znak = s[1];
-        if ((s.length == 3) & (znak.matches("^[+,-,*,/]+$"))) {
-            if (num_1.matches("^[I,II,III,IV,V,VI,VII,VIII,IX,X]+$") & num_2.matches("^[I,II,III,IV,V,VI,VII,VIII,IX,X]+$")) {
-                System.out.println(getRomeRusult(num_1, num_2, znak));
-            } else {
-                System.out.println(getResult(num_1, num_2, znak));
+        for (int i = 1; i <= 3; ) {
+            if ((s.length == 3) & (znak.matches("^[-,+,*,/]+$"))) {
+                if (num_1.matches("^[I,II,III,IV,V,VI,VII,VIII,IX,X]+$") & num_2.matches("^[I,II,III,IV,V,VI,VII,VIII,IX,X]+$")) {
+                    return getRomeRusult(num_1, num_2, znak);
+                } else {
+                    return getResult(num_1, num_2, znak);
+                }
             }
+            throw new Exception();
         }
-        throw new Exception();
+        return null;
     }
 
-    static int getResult(String num_1, String num_2, String znak) throws Exception {
+    static String getResult(String num_1, String num_2, String znak) throws Exception {
         int a = Integer.parseInt(num_1);
         int b = Integer.parseInt(num_2);
         if ((a >= 1 & a <= 10) & (b >= 1 & b <= 10)) {
@@ -40,12 +52,13 @@ public class Main {
                     res = a / b;
                     break;
             }
-            return res;
+            return Integer.toString(res);
         }
         throw new Exception();
     }
-    static String getRomeRusult(String num_1, String num_2, String znak) {
-        String[] roman = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+
+    static String getRomeRusult(String num_1, String num_2, String znak) throws Exception {
+        String[] roman = {"Ну нет нуля в римских", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
                 "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
                 "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
                 "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
@@ -57,21 +70,25 @@ public class Main {
         int a = Integer.parseInt(valueOf(Arrays.asList(numRoman).indexOf(num_1)));
         int b = Integer.parseInt(valueOf(Arrays.asList(numRoman).indexOf(num_2)));
         int total = 1;
-        switch (znak) {
-            case "-":
-                total = a - b;
-                break;
-            case "+":
-                total = a + b;
-                break;
-            case "*":
-                total = a * b;
-                break;
-            case "/":
-                total = a / b;
-                break;
+        String s = "^[-,+,*,/]+$";
+        if ((s.equals(znak))) ;
+        {
+            switch (znak) {
+                case "-":
+                    total = a - b;
+                    break;
+                case "+":
+                    total = a + b;
+                    break;
+                case "*":
+                    total = a * b;
+                    break;
+                case "/":
+                    total = a / b;
+                    break;
+            }
+            String totalRoman = copyValueOf(Arrays.asList(roman).get(total).toCharArray());
+            return totalRoman;
         }
-        String totalRoman = copyValueOf(Arrays.asList(roman).get(total).toCharArray());
-        return totalRoman;
     }
 }
